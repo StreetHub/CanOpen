c04d613927d0b260f2459cfc87cb37031973324b
alexloi <alex@roboboogie.us>
2014-05-07 15:29:15 +0100

    running in bg now

--- src/ios/CanOpen.m
+++ src/ios/CanOpen.m
@@ -12,18 +12,20 @@
 @implementation CanOpen
 
 -(void) appCanOpen:(CDVInvokedUrlCommand*)command {
-    CDVPluginResult* pluginResult = nil;
-    
-    NSString* appUrl = [command.arguments objectAtIndex:0];
-    
-    if (appUrl != nil && [appUrl length] > 0) {
-        NSURL *url = [NSURL URLWithString: appUrl];
-        BOOL result = [[UIApplication sharedApplication] canOpenURL: url];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:result];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* pluginResult = nil;
+        
+        NSString* appUrl = [command.arguments objectAtIndex:0];
+        
+        if (appUrl != nil && [appUrl length] > 0) {
+            NSURL *url = [NSURL URLWithString: appUrl];
+            BOOL result = [[UIApplication sharedApplication] canOpenURL: url];
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:result];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+        }
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 @end
